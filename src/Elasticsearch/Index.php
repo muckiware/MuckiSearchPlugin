c526f462f9bef1280d03bb23c62df417e483b701
Torsten Freyda <torstenfreyda@webworker.com>
2023-12-18 19:00:57 +0100

    Added server indexing method

---
+++ src/Elasticsearch/Index.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * MuckiSearchPlugin plugin
+ *
+ *
+ * @category   Muckiware
+ * @package    MuckiSearch
+ * @copyright  Copyright (c) 2023 by Muckiware
+ *
+ * @author     Muckiware
+ *
+ */
+
+namespace MuckiSearchPlugin\Elasticsearch;
+
+use Elastic\Elasticsearch\Exception\ClientResponseException;
+use Elastic\Elasticsearch\Exception\ServerResponseException;
+use Elastic\Elasticsearch\Response\Elasticsearch;
+use Http\Promise\Promise;
+use Psr\Log\LoggerInterface;
+use Elastic\Elasticsearch\ClientBuilder;
+use Elastic\Elasticsearch\ClientInterface;
+use Elastic\Elasticsearch\Exception\AuthenticationException;
+
+use MuckiSearchPlugin\Elasticsearch\Client as ElasticsearchClient;
+
+class Index
+{
+    public function __construct(
+        protected ElasticsearchClient $elasticsearchClient,
+        protected LoggerInterface $logger
+    )
+    {}
+
+    /**
+     *
+     */
+    public function indexing(array $params): ?array
+    {
+        try {
+            return $this->elasticsearchClient
+                ->getClient()
+                ->index($params)
+                ->asArray();
+        } catch (ClientResponseException $clientEx) {
+            $this->logger->error($clientEx->getMessage());
+        } catch (ServerResponseException $resEx) {
+            $this->logger->error($resEx->getMessage());
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+        }
+
+        return null;
+    }
+}
